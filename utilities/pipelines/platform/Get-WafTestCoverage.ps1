67ef3bf0e205e8f2d41dce6d24e7d129b5fd854a
Rainer Halanek <rahalan@microsoft.com>
2025-04-02 17:21:31 +0200

    test

--- utilities/pipelines/platform/Get-WafTestCoverage.ps1
+++ utilities/pipelines/platform/Get-WafTestCoverage.ps1
@@ -8,14 +8,8 @@ This function will download the custom baseline WAF test results for the last mo
 .PARAMETER Repo
 Mandatory. The name of the respository to scan. Needs to have the structure "<owner>/<repositioryName>", like 'Azure/bicep-registry-modules/'
 
-.PARAMETER LimitNumberOfRuns
-Optional. Number of recent runs to scan for failed runs. Default is 100.
-
-.PARAMETER LimitInDays
-Optional. Only runs in the past selected days will be analyzed. Default is 2.
-
 .EXAMPLE
-Get-WafTestCoverage -Repo 'owner/repo01' -LimitNumberOfRuns 100 -LimitInDays 2
+Get-WafTestCoverage -Repo 'owner/repo01'
 
 Gets all custom baseline WAF test results for the last 100 workflow runs in the repository 'owner/repo01' that happened in the last 2 days.
 
@@ -25,43 +19,28 @@ Will be triggered by the workflow platform.get-waf-test-coverage.yml
 function Get-WafTestCoverage {
     param (
         [Parameter(Mandatory = $true)]
-        [string] $Repo,
-
-        [Parameter(Mandatory = $false)]
-        [int] $LimitNumberOfRuns = 100,
-
-        [Parameter(Mandatory = $false)]
-        [int] $LimitInDays = 2
+        [string] $Repo
     )
 
-    $runs = gh run list --json 'number,name,databaseId,headBranch,startedAt' --limit $LimitNumberOfRuns --repo $Repo | ConvertFrom-Json -Depth 100
-    $uniqueRuns = @()
+    # ATTENTION: change to main branch when creating the PR
+    $runId = gh run list --repo $repo --workflow 'platform.check.psrule.yml' --branch 'users/rahalan/BaselineCoverage3' --limit 1 --json 'databaseId' `
+    | ConvertFrom-Json -Depth 100 `
+    | Select-Object -ExpandProperty 'databaseId'
 
-    foreach ($run in $runs | Sort-Object -Property name, number -Descending) {
-        $runStartTime = [Datetime]::ParseExact($run.startedAt, 'MM/dd/yyyy HH:mm:ss', $null)
+    gh run download $runId --repo $Repo
 
-        # ATTENTION: change to -eq main when creating the PR
-        if (($run.headBranch -ne 'main') -and ($runStartTime -gt (Get-Date).AddDays(-$LimitInDays)) -and ($run.name.StartsWith('avm.'))) {
-            if ($uniqueRuns -notcontains $run.name) {
-                $uniqueRuns += $run.name
-                Write-Host "Downloading artifacts for run: $($run.name) $($run.databaseId)"
-                gh run download $run.databaseId -p 'CB.AVM.WAF*' --repo $Repo
-            }
-        }
-    }
+    $directory = Get-ChildItem -Filter 'avm-res-PSRule-output.csv' | Select-Object -Last 1
+    $csv = Get-ChildItem -Path $directory.FullName
 
-    $directories = Get-ChildItem -Filter 'CB.AVM.WAF*'
+    if ($null -ne $csv) {
+        $tests = Import-Csv -Path $csv.FullName
 
-    foreach ($directory in $directories) {
-        $csv = Get-ChildItem -Path $directory.FullName
-
-        if ($null -ne $csv) {
-            $tests = Import-Csv -Path $csv.FullName
-
-            if ($tests.Count -gt 0) {
-                Write-Host $directory.Name
-                $tests | Format-Table
-            }
+        if ($tests.Count -gt 0) {
+            $tests `
+            | Where-Object { $_.TargetType -ne 'Microsoft.Resources/deployments' -and $_.TargetType -ne 'Microsoft.Resources/deploymentScripts' } `
+            | Select-Object -Property 'TargetType', 'RuleName', 'Outcome' -Unique `
+            | Measure-Object
+            | Format-Table
         }
     }
 }
